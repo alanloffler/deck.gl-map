364c6a3c7b31d881a3cdbc95eaa6ff9224bc9178
alanloffler <alanmatiasloffler@gmail.com>
2025-04-10 18:19:02 -0300

    Redesign UI with Card components and improve layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import { useState } from "react";
 import "./App.css";
 import { MyMap } from "./components/MyMap";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card";
 
 export default function App() {
   const [info, setInfo] = useState(undefined);
 
   return (
-    <main className="flex flex-col space-y-4">
-      <h1 className="font-semibold text-2xl">Redes de agua potable</h1>
-      <h2 className="font-medium text-lg">Visualización de conexiones</h2>
-      <aside className="flex flex-row gap-4">
-        <section className="w-3/4">
-          <MyMap setInfo={setInfo} />
-        </section>
-        <section className="w-1/4">
-          <h5 className="font-medium text-base">Datos de la selección</h5>
+    <main className="flex flex-row gap-6">
+      <Card className="w-2/3">
+        <CardHeader>
+          <CardTitle>Redes de agua potable</CardTitle>
+          <CardDescription>Visualización de conexiones</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <MyMap className="w-full" setInfo={setInfo} />
+        </CardContent>
+      </Card>
+      <Card className="w-1/3">
+        <CardHeader>
+          <CardTitle>Datos de la selección</CardTitle>
+        </CardHeader>
+        <CardContent>
           <div className="text-base font-normal">
             {info ? (
               <p>{info}</p>
@@ -22,8 +35,8 @@ export default function App() {
               <p>Realiza la selección de algún elemento del mapa</p>
             )}
           </div>
-        </section>
-      </aside>
+        </CardContent>
+      </Card>
     </main>
   );
 }
