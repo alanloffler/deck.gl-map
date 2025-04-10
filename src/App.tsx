6d5456f18fbd0215498b8f30b0c9cf67ba252df5
alanloffler <alanmatiasloffler@gmail.com>
2025-04-10 15:49:13 -0300

    Add info state to display map selection details

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 import { MyMap } from "./components/MyMap";
 
 export default function App() {
+  const [info, setInfo] = useState(undefined);
+
   return (
     <main className="flex flex-col space-y-4">
       <h1 className="font-semibold text-lg">Visualización de conexiones</h1>
       <section>Redes de agua potable</section>
       <div>
-        <MyMap />
+        <MyMap setInfo={setInfo} />
       </div>
+      <section>{info && <p>{info}</p>}</section>
     </main>
   );
 }
