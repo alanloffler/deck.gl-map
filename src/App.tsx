6b3f2819b89b609e1e417543e273522048cc58e5
alanloffler <alanmatiasloffler@gmail.com>
2025-04-10 17:38:26 -0300

    Improve UI layout and information display in water network map

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,23 @@ export default function App() {
 
   return (
     <main className="flex flex-col space-y-4">
-      <h1 className="font-semibold text-lg">Visualización de conexiones</h1>
-      <section>Redes de agua potable</section>
-      <div>
-        <MyMap setInfo={setInfo} />
-      </div>
-      <section>{info && <p>{info}</p>}</section>
+      <h1 className="font-semibold text-2xl">Redes de agua potable</h1>
+      <h2 className="font-medium text-lg">Visualización de conexiones</h2>
+      <aside className="flex flex-row gap-4">
+        <section className="w-3/4">
+          <MyMap setInfo={setInfo} />
+        </section>
+        <section className="w-1/4">
+          <h5 className="font-medium text-base">Datos de la selección</h5>
+          <div className="text-base font-normal">
+            {info ? (
+              <p>{info}</p>
+            ) : (
+              <p>Realiza la selección de algún elemento del mapa</p>
+            )}
+          </div>
+        </section>
+      </aside>
     </main>
   );
 }
