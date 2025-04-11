e41be0ae5d55e5d7976750854970825fde1cbdd0
alanloffler <alanmatiasloffler@gmail.com>
2025-04-11 20:04:34 -0300

    Add React Router with MapLibre routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { MapLibre } from "./MapLibre.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route index element={<MapLibre />} />
+          <Route path="/maplibre" element={<MapLibre />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
