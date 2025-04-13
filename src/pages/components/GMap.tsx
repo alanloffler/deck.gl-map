09a6881ec7b1f0a9b7715e9293243d3f383599ab
alanloffler <alanmatiasloffler@gmail.com>
2025-04-13 20:29:51 -0300

    Consume data locally

--- src/pages/components/GMap.tsx
+++ src/pages/components/GMap.tsx
@@ -4,24 +4,28 @@ import { GeoJsonLayer } from "deck.gl";
 import { useEffect, useState } from "react";
 import { DeckGlOverlay } from "./DeckGlOverlay";
 
-const DATA_URL =
-  "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/bart.geo.json";
+// const DATA_URL =
+// ("https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/bart.geo.json");
+
+import watter from "../../data/watter.json";
 
 export function GMap() {
   const [data, setData] = useState<GeoJSON | null>(null);
 
   useEffect(() => {
-    fetch(DATA_URL)
-      .then((res) => res.json())
-      .then((data) => setData(data as GeoJSON));
+    setData(watter as GeoJSON);
+    // fetch(DATA_URL)
+    //   .then((res) => res.json())
+    //   .then((data) => setData(data as GeoJSON));
   }, []);
 
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
       <Map
         mapId={import.meta.env.VITE_GOOGLE_MAPS_ID}
-        defaultCenter={{ lat: 37.74, lng: -122.4 }}
-        defaultZoom={11}
+        // defaultCenter={{ lat: 37.74, lng: -122.4 }}
+        defaultCenter={{ lng: -54.566963, lat: -25.973053 }}
+        defaultZoom={16}
         gestureHandling={"greedy"}
         disableDefaultUI={false}
         colorScheme="LIGHT"
@@ -45,7 +49,7 @@ function getDeckGlLayers(data: GeoJSON | null) {
       filled: true,
       extruded: true,
       pointType: "circle",
-      lineWidthScale: 20,
+      lineWidthScale: 5,
       lineWidthMinPixels: 4,
       getFillColor: [160, 160, 180, 200],
       getLineColor: (f: Feature) => {
@@ -54,7 +58,7 @@ function getDeckGlLayers(data: GeoJSON | null) {
 
         return hex.match(/[0-9a-f]{2}/g)!.map((x: string) => parseInt(x, 16));
       },
-      getPointRadius: 200,
+      getPointRadius: 20,
       getLineWidth: 1,
       getElevation: 30,
     }),
