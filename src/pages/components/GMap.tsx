328bc5288b43a338e1b58e9be19e318184e772d6
alanloffler <alanmatiasloffler@gmail.com>
2025-04-12 23:41:24 -0300

    Use BART demo data instead of local water data

--- src/pages/components/GMap.tsx
+++ src/pages/components/GMap.tsx
@@ -1,49 +1,20 @@
+import type { Feature, GeoJSON } from "geojson";
 import { APIProvider, Map } from "@vis.gl/react-google-maps";
-import { type Feature, type GeoJSON } from "geojson";
 import { GeoJsonLayer } from "deck.gl";
 import { useEffect, useState } from "react";
-
 import { DeckGlOverlay } from "./DeckGlOverlay";
 
-// const DATA_URL =
-//   "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/bart.geo.json";
-import watterData from "../../data/data.json";
+const DATA_URL =
+  "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/bart.geo.json";
+
 export function GMap() {
   const [data, setData] = useState<GeoJSON | null>(null);
 
   useEffect(() => {
-    setData(watterData as GeoJSON);
-    // fetch(DATA_URL)
-    //   .then((res) => res.json())
-    //   .then((data) => setData(data as GeoJSON));
+    fetch(DATA_URL)
+      .then((res) => res.json())
+      .then((data) => setData(data as GeoJSON));
   }, []);
-
-  function getDeckGlLayers(data: GeoJSON | null) {
-    if (!data) return [];
-
-    return [
-      new GeoJsonLayer({
-        id: "geojson-layer",
-        data,
-        stroked: false,
-        filled: true,
-        extruded: true,
-        pointType: "circle",
-        lineWidthScale: 20,
-        lineWidthMinPixels: 4,
-        getFillColor: [160, 160, 180, 200],
-        getLineColor: (f: Feature) => {
-          const hex = f?.properties?.color;
-          if (!hex) return [0, 0, 0];
-
-          return hex.match(/[0-9a-f]{2}/g)!.map((x: string) => parseInt(x, 16));
-        },
-        getPointRadius: 200,
-        getLineWidth: 1,
-        getElevation: 30,
-      }),
-    ];
-  }
 
   return (
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
@@ -58,4 +29,31 @@ export function GMap() {
       </Map>
     </APIProvider>
   );
+}
+
+function getDeckGlLayers(data: GeoJSON | null) {
+  if (!data) return [];
+
+  return [
+    new GeoJsonLayer({
+      id: "geojson-layer",
+      data,
+      stroked: false,
+      filled: true,
+      extruded: true,
+      pointType: "circle",
+      lineWidthScale: 20,
+      lineWidthMinPixels: 4,
+      getFillColor: [160, 160, 180, 200],
+      getLineColor: (f: Feature) => {
+        const hex = f?.properties?.color;
+        if (!hex) return [0, 0, 0];
+
+        return hex.match(/[0-9a-f]{2}/g)!.map((x: string) => parseInt(x, 16));
+      },
+      getPointRadius: 200,
+      getLineWidth: 1,
+      getElevation: 30,
+    }),
+  ];
 }
