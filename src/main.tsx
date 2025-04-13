3397e70b2c1c97771fa677ef9a014d672eece3bf
alanloffler <alanmatiasloffler@gmail.com>
2025-04-12 23:39:18 -0300

    Disable React StrictMode (Google Map do not work with it)

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,21 @@
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { GoogleMap } from "./pages/GoogleMap.tsx";
 import { MapLibre } from "./pages/MapLibre.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<App />}>
-          <Route index element={<MapLibre />} />
-          <Route path="/maplibre" element={<MapLibre />} />
-          <Route path="/googlemap" element={<GoogleMap />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+  // <StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route element={<App />}>
+        <Route index element={<MapLibre />} />
+        <Route path="/maplibre" element={<MapLibre />} />
+        <Route path="/googlemap" element={<GoogleMap />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  // {/* </StrictMode>, */}
 );
