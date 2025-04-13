1a7fe8877e5e9a9ce246dd695871eb00db9c9321
alanloffler <alanmatiasloffler@gmail.com>
2025-04-12 21:20:42 -0300

    Add z-index to fixed nav to prevent overlay issues

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from "react-router";
 export default function App() {
   return (
     <main className="flex flex-col space-y-6">
-      <nav className="fixed top-0 left-0 flex h-16 w-full items-center space-x-6 border-b bg-white px-6 font-semibold">
+      <nav className="fixed top-0 left-0 z-50 flex h-16 w-full items-center space-x-6 border-b bg-white px-6 font-semibold">
         <Link to="/maplibre">MapLibre</Link>
         <Link to="/googlemap">Google Map</Link>
       </nav>
