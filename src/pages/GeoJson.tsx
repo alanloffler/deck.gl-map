761d9b22fbdae276e1279e69c2da20b5ac250536
alanloffler <alanmatiasloffler@gmail.com>
2025-04-19 20:03:45 -0300

    Add GeoJson component page with basic card structure

---
+++ src/pages/GeoJson.tsx
@@ -0,0 +1,12 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export function GeoJson() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>GeoJson map</CardTitle>
+      </CardHeader>
+      <CardContent>Map here</CardContent>
+    </Card>
+  );
+}
