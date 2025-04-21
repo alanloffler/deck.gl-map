eb9ddb33ae76170d126bbb2ca8fd92fadbdda942
alanloffler <alanmatiasloffler@gmail.com>
2025-04-21 11:37:13 -0300

    Replace MapLibre with Google Maps as default map provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,17 @@
-import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router";
 // import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import "./index.css";
 import App from "./App.tsx";
-import { GeoJson } from "./pages/GeoJson.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
 import { GoogleMap } from "./pages/GoogleMap.tsx";
-import { MapLibre } from "./pages/MapLibre.tsx";
+import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
   <BrowserRouter>
     <Routes>
       <Route element={<App />}>
-        <Route index element={<MapLibre />} />
-        <Route path="/maplibre" element={<MapLibre />} />
+        <Route index element={<GoogleMap />} />
         <Route path="/googlemap" element={<GoogleMap />} />
-        <Route path="/geojson" element={<GeoJson />} />
       </Route>
     </Routes>
   </BrowserRouter>,
