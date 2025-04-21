31627f38de4fb2cf59e947f096a2c8180ee6bce9
alanloffler <alanmatiasloffler@gmail.com>
2025-04-21 00:16:59 -0300

    Use DeckProps type instead of custom type for DeckGLOverlay

--- src/pages/components/DeckGlOverlay.tsx
+++ src/pages/components/DeckGlOverlay.tsx
@@ -1,15 +1,9 @@
 import { GoogleMapsOverlay } from "@deck.gl/google-maps";
-import { type LayersList, type PickingInfo } from "deck.gl";
+import { DeckProps } from "deck.gl";
 import { useEffect, useMemo } from "react";
 import { useMap } from "@vis.gl/react-google-maps";
-import type { ITooltip } from "@/interfaces/tooltip.type";
 
-type IDeckGLOverlay = {
-  layers?: LayersList;
-  getTooltip?: (object: PickingInfo) => ITooltip | null;
-};
-
-export const DeckGLOverlay = ({ layers, getTooltip }: IDeckGLOverlay) => {
+export const DeckGLOverlay = ({ layers, getTooltip }: DeckProps) => {
   const deck = useMemo(
     () =>
       new GoogleMapsOverlay({
