fae13a8b09e2d1215396b3d6d09377f58846dde8
alanloffler <alanmatiasloffler@gmail.com>
2025-04-21 12:49:50 -0300

    Implement lazy loading with Suspense for GoogleMap component

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,39 @@
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
-import { GoogleMap } from "./pages/GoogleMap.tsx";
 import { createRoot } from "react-dom/client";
+import { lazy, Suspense } from "react";
+// import { GoogleMap } from "./pages/GoogleMap.tsx";
+import { Loading } from "./components/Loading.tsx";
+const GoogleMap = lazy(() => import("./pages/GoogleMap.tsx"));
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
   <BrowserRouter>
     <Routes>
-      <Route element={<App />}>
-        <Route index element={<GoogleMap />} />
-        <Route path="/googlemap" element={<GoogleMap />} />
+      <Route
+        element={
+          <Suspense fallback={<Loading />}>
+            <App />
+          </Suspense>
+        }
+      >
+        <Route
+          index
+          element={
+            <Suspense fallback={<Loading />}>
+              <GoogleMap />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/googlemap"
+          element={
+            <Suspense fallback={<Loading />}>
+              <GoogleMap />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   </BrowserRouter>,
