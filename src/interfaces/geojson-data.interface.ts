099eeeb01e345f755cbc56b0aed282fe0484c2cb
alanloffler <alanmatiasloffler@gmail.com>
2025-04-23 16:12:37 -0300

    Add optional icon field to GeoJSON details

--- src/interfaces/geojson-data.interface.ts
+++ src/interfaces/geojson-data.interface.ts
@@ -2,14 +2,10 @@ export interface IGeoJsonData {
   color: string;
   details: {
     district: string;
+    icon?: string;
     id: string;
     street: string;
   };
   name: string;
-  type:
-    | "connection"
-    | "marker"
-    | "network"
-    | "main-network"
-    | "secondary-network";
+  type: "connection" | "marker" | "network" | "main-network" | "secondary-network";
 }
