13baf83947fe4afff25bdffd2f1c5e4f06b02488
alanloffler <alanmatiasloffler@gmail.com>
2025-04-24 09:17:09 -0300

    Rename TestMap component to Networks and update route path

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { lazy, Suspense } from "react";
 // import { GoogleMap } from "./pages/GoogleMap.tsx";
 import { Loading } from "./components/Loading.tsx";
 const GoogleMap = lazy(() => import("./pages/GoogleMap.tsx"));
-const TestMap = lazy(() => import("./pages/TestMap.tsx"));
+const Networks = lazy(() => import("./pages/Networks.tsx"));
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
@@ -37,10 +37,10 @@ createRoot(document.getElementById("root")!).render(
           }
         />
         <Route
-          path="/test"
+          path="/redes"
           element={
             <Suspense fallback={<Loading />}>
-              <TestMap />
+              <Networks />
             </Suspense>
           }
         />
