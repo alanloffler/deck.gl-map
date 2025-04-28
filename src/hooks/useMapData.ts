9b4ee18d1c8ff5a15382d6d745544a2772a5555c
alanloffler <alanmatiasloffler@gmail.com>
2025-04-27 21:53:32 -0300

    Add useMapData hook for processing GeoJSON data

---
+++ src/hooks/useMapData.ts
@@ -0,0 +1,37 @@
+import type { Feature, FeatureCollection, Geometry, Point } from "geojson";
+import type { IGeoJsonData } from "@/interfaces/geojson-data.interface";
+import { useEffect, useState } from "react";
+
+// GeoJSON data
+import testData from "@/data/test-data.json";
+
+export function useMapData() {
+  const [geoJsonData, setGeoJsonData] = useState<FeatureCollection<Geometry, IGeoJsonData> | null>(null);
+  const [textData, setTextData] = useState<{ id: string; name: string; coordinates: number[] }[] | undefined>(
+    undefined,
+  );
+
+  useEffect(() => {
+    setGeoJsonData(testData as FeatureCollection<Geometry, IGeoJsonData>);
+  }, []);
+
+  useEffect(() => {
+    geoJsonData?.features.forEach((feature, index) => {
+      feature.properties.id = index;
+    });
+
+    const filterFeatures = geoJsonData?.features.filter(
+      (feature) => feature.properties.type === "connection",
+    ) as Feature<Point, IGeoJsonData>[];
+
+    const textFeatures = filterFeatures?.map((feat) => ({
+      coordinates: feat.geometry.coordinates,
+      id: feat.properties.details.id,
+      name: feat.properties.details.id,
+    }));
+
+    setTextData(textFeatures);
+  }, [geoJsonData?.features]);
+
+  return { geoJsonData, textData };
+}
