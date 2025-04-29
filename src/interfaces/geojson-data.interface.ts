90b6a7436065f048a41b521211716d527e811c4a
alanloffler <alanmatiasloffler@gmail.com>
2025-04-29 10:09:33 -0300

    Add title field to IGeoJsonData properties interface

--- src/interfaces/geojson-data.interface.ts
+++ src/interfaces/geojson-data.interface.ts
@@ -6,6 +6,7 @@ export interface IGeoJsonData {
     icon?: string;
     id: string;
     street: string;
+    title: string;
   };
   name: string;
   type: "connection" | "marker" | "network" | "main-network" | "secondary-network";
