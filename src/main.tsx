b760960f018aab811cd5e9c7fe803fbbe7b4aa13
alanloffler <alanmatiasloffler@gmail.com>
2025-04-29 16:06:26 -0300

    Remove GoogleMap component and route

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,7 @@ import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { createRoot } from "react-dom/client";
 import { lazy, Suspense } from "react";
-// import { GoogleMap } from "./pages/GoogleMap.tsx";
 import { Loading } from "./components/Loading.tsx";
-const GoogleMap = lazy(() => import("./pages/GoogleMap.tsx"));
 const Networks = lazy(() => import("./pages/Networks.tsx"));
 
 createRoot(document.getElementById("root")!).render(
@@ -25,14 +23,6 @@ createRoot(document.getElementById("root")!).render(
           element={
             <Suspense fallback={<Loading />}>
               <Networks />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/googlemap"
-          element={
-            <Suspense fallback={<Loading />}>
-              <GoogleMap />
             </Suspense>
           }
         />
